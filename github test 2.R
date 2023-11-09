a36e523778d1ae833741f8adfe00424c34c8a768
Anthony <adant@arizona.edu>
2023-11-09 09:15:16 -0800

    try this

--- github test 2.R
+++ github test 2.R
@@ -1,1 +1,10 @@
-We are testing again!
+#We are testing again!
+  
+  ## Notes from Sabrina 11/9/2023
+  ## steps to get to github
+  ##1. Save first!
+  #### 2.Stage them by checking off the box
+  ###3.Commit-> need a message ( I fixed this bug)
+  ###4 Push
+  ##however this can be hectic
+## Usually goes 1) Pres A- control repo, 2)Pers B, 
